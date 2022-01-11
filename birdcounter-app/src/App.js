28a9cfaa9e57fefcfac5c37b5f24bb2484dd33dc
E-K8 <ekatplat@gmail.com>
2022-01-11 18:49:41 +0000

    add style to header and total counter

--- birdcounter-app/src/App.js
+++ birdcounter-app/src/App.js
@@ -7,25 +7,40 @@ import swanPic from "./Public/bird_swan.png";
 import { BirdSection } from "./Components/BirdSection";
 
 function App() {
-    const [totalBirds, setTotalBirds] = useState(0);
+  const [totalBirds, setTotalBirds] = useState(0);
 
-    function addToTotal() { 
-        setTotalBirds(totalBirds + 1)
-      }
-    
-    function deductTotal() {
-        return totalBirds === 0 ? null : setTotalBirds(totalBirds - 1)
-    }
+  function addToTotal() {
+    setTotalBirds(totalBirds + 1);
+  }
 
-  return ( 
+  function deductTotal() {
+    return totalBirds === 0 ? null : setTotalBirds(totalBirds - 1);
+  }
+
+  return (
     <div className="App">
-      <h1>Bird counter</h1>
-      <h1>🦆🦢🕊️🐧</h1>    
-      <h2>Total Birds: {totalBirds}</h2>
+      <h1 className="header-title">Bird counter</h1>
+      <h1>🦆🦢🕊️🐧</h1>
+      <h2 className="total-counter">Total Birds: {totalBirds}</h2>
       <div className="gallery">
-      <BirdSection image={magpiePic} bird="Magpie" addTotalBirds={addToTotal} deductTotalBirds={deductTotal} />
-      <BirdSection image={cootPic} bird="Coot" addTotalBirds={addToTotal} deductTotalBirds={deductTotal}/>
-      <BirdSection image={swanPic} bird="Swan" addTotalBirds={addToTotal} deductTotalBirds={deductTotal}/>
+        <BirdSection
+          image={magpiePic}
+          bird="Magpie"
+          addTotalBirds={addToTotal}
+          deductTotalBirds={deductTotal}
+        />
+        <BirdSection
+          image={cootPic}
+          bird="Coot"
+          addTotalBirds={addToTotal}
+          deductTotalBirds={deductTotal}
+        />
+        <BirdSection
+          image={swanPic}
+          bird="Swan"
+          addTotalBirds={addToTotal}
+          deductTotalBirds={deductTotal}
+        />
       </div>
     </div>
   );
